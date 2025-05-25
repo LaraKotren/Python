0e5ab310147397e47e1dbbbbe1ceddac7389dfe3
LaraKotren <lauramihajlova315@gmail.com>
2025-05-25 21:20:43 +0300

    Add files via upload

---
+++ The prime number generator.py
@@ -0,0 +1,18 @@
+﻿import math
+
+def prime_numbers():
+    num = 2
+    while True:
+        is_prime = True
+        # Проверяем делители от 2 до квадратного корня из num
+        for i in range(2, int(math.sqrt(num)) + 1):
+            if num % i == 0:
+                is_prime = False
+                break
+        if is_prime:
+            yield num
+        num += 1
+
+primes = prime_numbers()
+for _ in range(10):
+    print(next(primes))
