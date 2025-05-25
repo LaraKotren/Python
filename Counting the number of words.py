902728657a9c89149e7c950a681e406f6a027581
LaraKotren <lauramihajlova315@gmail.com>
2025-05-25 20:59:34 +0300

    Add files via upload

---
+++ Counting the number of words.py
@@ -0,0 +1,14 @@
+﻿text = input("Введите строку: ").lower().split()  
+
+# Создаём пустой словарь для хранения количества повторений каждого слова
+word_counts = {}  
+
+for word in text:  
+    if word in word_counts:  
+        word_counts[word] += 1  
+    else:  
+        word_counts[word] = 1  
+
+# Выводим результат в формате "слово: количество повторений"
+for word, count in word_counts.items():  
+    print(f"{word}: {count}")  
