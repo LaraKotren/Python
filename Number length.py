7a65b5b8ac39649efec9afbda98e5735aa832e63
LaraKotren <lauramihajlova315@gmail.com>
2025-05-25 20:12:12 +0300

    Add files via upload

---
+++ Number length.py
@@ -0,0 +1,12 @@
+﻿while True:
+    user_input = input("Введите число: → ")
+    
+    if user_input == "exit":
+        print("Выход из программы...")
+        break
+    
+    if user_input.lstrip('-').isdigit():
+        num_digits = len(user_input.lstrip('-'))
+        print(f"В этом числе {num_digits} цифры.")
+    else:
+        print("Ошибка: данные не являются числом.")
