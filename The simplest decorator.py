47024731717f535ffec4abaadb9da84b2ea4f7e0
LaraKotren <lauramihajlova315@gmail.com>
2025-05-25 21:30:37 +0300

    Add files via upload

---
+++ The simplest decorator.py
@@ -0,0 +1,17 @@
+﻿def debug_decorator(func):
+    def wrapper(*args, **kwargs):
+        print(f"Функция {func.__name__} вызвана с аргументами:")
+        print(f"Позиционные аргументы: {args}")
+        print(f"Именованные аргументы: {kwargs}")
+        
+        result = func(*args, **kwargs)
+        
+        print(f"Площадь прямоугольника: {result}")
+        return result
+    return wrapper
+
+@debug_decorator
+def calculate_area(length, width):
+    return length * width
+
+calculate_area(5, 10)
