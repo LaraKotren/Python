213465daf59ddff50829bcf3a1ac4f2a1c9a8943
LaraKotren <lauramihajlova315@gmail.com>
2025-05-25 21:28:12 +0300

    Add files via upload

---
+++ Example of work.py
@@ -0,0 +1,12 @@
+﻿def append_to_file(text, filename):
+    # Добавляем текст в файл с новой строки
+    with open(filename, 'a', encoding='utf-8') as f:
+        f.write(text + '\n')
+    
+    # Читаем файл и выводим четные строки
+    with open(filename, 'r', encoding='utf-8') as f:
+        lines = f.readlines()
+        print("Четные строки файла:")
+        for index, line in enumerate(lines, start=1):
+            if index % 2 == 0:
+                print(line.strip())
